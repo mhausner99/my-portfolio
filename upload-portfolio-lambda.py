43eeae6ff52f98617c6ce4208036ab479933905a
Max Hausner <janhaus@f018983ef230.ant.amazon.com>
2019-11-08 16:53:10 +0100

    Adding the upload script 4

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -16,5 +16,6 @@ build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm)
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]}
+        portfolio_bucket.upload_fileobj(obj,nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        #portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
